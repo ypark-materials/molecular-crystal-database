1e3e8a9aabc3f36a37b1c6a92d112e119f26e763
ypark-materials <ypark.materials@gmail.com>
2025-04-18 21:16:01 -0700

    Edit micromech.py: added function compat that calculates compatibility condition

--- module/micromech.py
+++ module/micromech.py
@@ -12,7 +12,7 @@ Contact: yunsu@ucsb.edu
 import numpy as np
 
 def defgrad(reflat, deflat):
-    """
+    '''
     Calculates the deformation gradient from the lattice vectors of the reference and deformed configuration
 
     Parameters:
@@ -31,7 +31,7 @@ def defgrad(reflat, deflat):
         F (ndarray [shape (3, 3)]):
             deformation gradient for the material 
             going from the reference configuration (reflat) to the deformed configuration (deflat)
-    """
+    '''
 
     F = deflat @ np.linalg.inv(reflat)
 
@@ -39,7 +39,7 @@ def defgrad(reflat, deflat):
 
 
 def streten(F):
-    """
+    '''
     Polar decomposes the deformation gradient to the stretch tensor (U) and rotation matrix (Q) [F=QU]
 
     Parameters:
@@ -51,7 +51,7 @@ def streten(F):
             stretch tensor from the deforamtion gradient (F)
         Q (ndarray [shape (3, 3)]):
             rotation matrix from the deformation gradient (F)
-    """
+    '''
 
     C = F.T @ F
 
@@ -70,10 +70,50 @@ def streten(F):
 
     return U, Q
 
-
 def defvol(F):
     pass
 def defare(F): 
     pass
 def defstr(F):
     pass
+
+def compat(F, G):
+    '''
+    Checks the compatibility condition (lam1 <= 1, lam2 = 1, lam3 >= 1)
+
+    Parameters:
+        F (ndarray [shape (3, 3)]):
+            deformation gradient for martensite variant I
+        G (ndarray [shape (3, 3)]):
+            deformation gradient for martensite variant J or austenite
+    Returns:
+        lam (ndarray [shape (1, 3)]):
+            eigenvalue for matrix C in ascending order
+        e (ndarray [shape (3, 3)]):
+            eigenvector for matrix C as column vectors associated with the eigenvalue
+    '''
+    #calculates C matrix
+    G_inv = np.linalg.inv(G)
+    C = G_inv.T @ F.T @ F @ G_inv
+    
+    #checks if C is not identity
+    if np.all(np.equal(C , np.eye(3))):
+        print('ERROR: C is tensor is an identity matrix')
+        return 0
+    
+    #calculates the eigenvale lam and vector e
+    lam, e = np.linalg.eig(C)
+    
+    #sorting eigenvector and values from low to high (ascending order)
+    idx = lam.argsort()
+    lam = lam[idx]
+    e = e[:,idx]
+
+    #checks if kinematic compatibility is met
+    if not (lam[0] <= 1 and lam[1] == 1 and lam[2] >= 1):
+        print('WARNING: eigenvalues does not satisfy kinematic compatibility')
+        print(' lam1 = ', lam[0])
+        print(' lam2 = ', lam[1])
+        print(' lam3 = ', lam[2] , '\n')
+
+    return lam, e
